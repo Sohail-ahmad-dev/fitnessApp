3cbeaf2355c94a2125e2b0b2877e0a35af05bffe
Shakil Ahmed <shakilmultan@gmail.com>
2023-01-11 19:16:07 +0500

    first update

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shakeel
+ * Date: 11/01/2023
+ * Time: 6:55 PM
+ */
+?>
+
+<footer class="main-footer">
+    <div class="auto-container">
+        <!--Upper-->
+        <div class="upper clearfix">
+            <h2 class="pull-left">GET <span>IN TOUCH</span></h2>
+            <div class="social-links pull-left">
+                <a href="#" class="icon fa fa-facebook-f wow fadeInRight" data-wow-delay="0s" data-wow-duration="1s" data-wow-offset="0"></a>
+                <a href="#" class="icon fa fa-twitter wow fadeInRight" data-wow-delay="0.2s" data-wow-duration="1s" data-wow-offset="0"></a>
+                <a href="#" class="icon fa fa-dribbble wow fadeInRight" data-wow-delay="0.4s" data-wow-duration="1s" data-wow-offset="0"></a>
+                <a href="#" class="icon fa fa-google wow fadeInRight" data-wow-delay="0.6s" data-wow-duration="1s" data-wow-offset="0"></a>
+                <a href="#" class="icon fa fa-pinterest wow fadeInRight" data-wow-delay="0.8s" data-wow-duration="1s" data-wow-offset="0"></a>
+                <a href="#" class="icon fa fa-instagram wow fadeInRight" data-wow-delay="1s" data-wow-duration="1s" data-wow-offset="0"></a>
+            </div>
+        </div>
+
+        <!--Lower-->
+        <div class="lower">
+            <div class="row clearfix">
+
+                <!--Copyright-->
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="footer-widget copyright wow fadeInUp" data-wow-delay="0.5s" data-wow-duration="1s" data-wow-offset="0">
+                        <h2><img src="/assets/images/logo-footer.png" alt="" title=""></h2>
+                        <p>&copy; 2015 STAYFIT - FITNESS &amp; GYM COMPANY</p>
+                        <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a> | <a href="#">FAQ</a>
+                    </div>
+                </div>
+
+                <!--Quick Links-->
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="footer-widget quick-links wow fadeInUp" data-wow-delay="1s" data-wow-duration="1s" data-wow-offset="0">
+                        <h2>Quick Links</h2>
+                        <div class="clearfix">
+                            <ul>
+                                <li><a href="#">Our Courses</a></li>
+                                <li><a href="#">Workouts</a></li>
+                                <li><a href="#">Coaches</a></li>
+                                <li><a href="#">Training Areas</a></li>
+                            </ul>
+                            <ul>
+                                <li><a href="#">Gym Cities</a></li>
+                                <li><a href="#">Pricings</a></li>
+                                <li><a href="#">Products</a></li>
+                                <li><a href="#">Sessions</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Popular Tags-->
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="footer-widget popular-tags wow fadeInUp" data-wow-delay="1.5s" data-wow-duration="1s" data-wow-offset="0">
+                        <h2>Popular Tags</h2>
+                        <a href="#">Fitness</a>
+                        <a href="#">Workout</a>
+                        <a href="#">Coach</a>
+                        <a href="#">Services</a>
+                        <a href="#">Training</a>
+                        <a href="#">Health</a>
+                        <a href="#">Running</a>
+                        <a href="#">Country</a>
+                        <a href="#">Claims</a>
+                    </div>
+                </div>
+
+                <!--Contact-->
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="footer-widget contact wow fadeInUp" data-wow-delay="2s" data-wow-duration="1s" data-wow-offset="0">
+                        <h2>Contact Us</h2>
+                        <ul>
+                            <li class="clearfix">
+                                <strong class="pull-left">Mail</strong>
+                                <p class="pull-right">STAYFIT@email.com</p>
+                            </li>
+                            <li class="clearfix">
+                                <strong class="pull-left">Phone</strong>
+                                <p class="pull-right">+61 123 456 789</p>
+                            </li>
+                            <li class="clearfix">
+                                <strong class="pull-left">Address</strong>
+                                <p class="pull-right">New York City, <br>Avenue Street 321 Melbourne 3000</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <!--Lower End-->
+
+    </div>
+    <!-- Back to top Button -->
+    <a href="#" class="back-to-top">
+        <i class="fa fa-angle-up"></i>
+    </a>
+</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shakeel
+ * Date: 11/01/2023
+ * Time: 6:55 PM
+ */
+?>
+
+<header class="main-header">
+    <div class="auto-container clearfix">
+
+        <!-- Logo -->
+        <div class="logo"><a href="#"><img src="/assets/images/logo.png" alt="Logo" title="StayFit"></a></div>
+
+        <!-- Main Menu -->
+        <nav class="main-menu">
+            <div class="navbar-header">
+                <!-- Toggle Button -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <div class="navbar-collapse collapse clearfix">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="current dropdown"><a href="index.html">Home</a>
+                        <ul class="submenu">
+                            <li><a href="index.html">Home Style One</a></li>
+                            <li><a href="index-2.html">Home Style Two</a></li>
+                            <li><a href="index-3.html">Home Style Three</a></li>
+                            <li><a href="index-one-page-version.html"> One Page Version</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#">Pages</a>
+                        <ul class="submenu">
+                            <li><a href="faq.html">FAQ</a></li>
+                            <li><a href="qa.html">Q & A</a></li>
+                            <li><a href="testimonials.html">Testimonials</a></li>
+                            <li class="dropdown"><a href="gallery-page-2-col.html">Gallery</a>
+                                <ul class="submenu">
+                                    <li><a href="gallery-page-2-col.html">Gallery Two Col</a></li>
+                                    <li><a href="gallery-page-3-col.html">Gallery Three Col</a></li>
+                                    <li><a href="gallery-page-3-col-full-width.html">Gallery Three Col Full Width</a></li>
+                                    <li><a href="gallery-page-4-col.html">Gallery Four Col</a></li>
+                                    <li><a href="gallery-page-4-col-full-width.html">Gallery Four Col Full Width</a></li>
+                                    <li><a href="gallery-page-6-col.html">Gallery Six Col</a></li>
+                                    <li><a href="gallery-page-6-col-full-width.html">Gallery Six Col Full Width</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class=" dropdown"><a href="about-us.html">About us</a>
+                        <ul class="submenu">
+                            <li><a href="our-team.html">Our Team</a></li>
+                            <li><a href="pricing.html">Our Pricing</a></li>
+
+                        </ul>
+                    </li>
+                    <li><a href="courses.html">Courses</a></li>
+                    <li class="dropdown"><a href="services.html">Services</a>
+                        <ul class="submenu">
+                            <li><a href="schedule.html">Our Schedule</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="blog.html">Blog</a>
+                        <ul class="submenu">
+                            <li><a href="blog-without-sidebar.html">Blog Full Width</a></li>
+                            <li><a href="blog.html">Blog Left Sidebar</a></li>
+                            <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                            <li><a href="blog-left-right-sidebar.html">Blog Left Right Sidebar</a></li>
+                            <li><a href="blog-detail.html">Single Post</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+        </nav>
+        <!-- Main Menu End-->
+
+    </div>
+
+    <div class="open-hours">
+        <div class="toggle-open-hours"><span class="fa fa-clock-o"></span> <span>Opening Hours</span> <br>Monday - Sunday 8:00 - 22:00</div>
+    </div>
+
+</header>
+
